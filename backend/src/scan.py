b8d245c88240719b4343c6bdda06d7bbb3c73a66
Galaxiumitas <143977201+Galaxiumitas@users.noreply.github.com>
2024-11-26 20:53:47 -0500

    Create scan.py

---
+++ backend/src/scan.py
@@ -0,0 +1,32 @@
+def scan(requests, start, direction, disk_size):
+    """
+    SCAN Implementation
+    
+    Parametros:
+    requests: Lista de solicitudes de cilindros.
+    start: Posición inicial del cabezal del disco.
+    direction: Dirección inicial del movimiento .
+    disk_size: Tamaño del disco.
+    
+    return: Orden de procesamiento y tiempo total de búsqueda.
+    """
+    requests.sort()
+    total_seek_time = 0
+    current_position = start
+    order = []
+
+    if direction == "izquierda":
+        left = [r for r in requests if r < current_position]
+        right = [r for r in requests if r >= current_position]
+        order = left[::-1] + right
+      
+    elif direction == "derecha":
+        left = [r for r in requests if r < current_position]
+        right = [r for r in requests if r >= current_position]
+        order = right + left[::-1]
+
+    for pos in order:
+        total_seek_time += abs(pos - current_position)
+        current_position = pos
+
+    return order, total_seek_time
